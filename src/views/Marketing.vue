4d82a5ae86cdb79f88994398a590d812721dae61
candy-dong <38694594+candy-dong@users.noreply.github.com>
2020-10-20 16:11:04 +0800

    营销探索 补充

--- src/views/Marketing.vue
+++ src/views/Marketing.vue
@@ -37,14 +37,30 @@
           <el-button  type="primary" plain @click="step2" style="margin-left: 10px">确 定</el-button>
         </el-form-item>
         <!-- 一键跑批 -->
+        <el-form-item  v-if="active===2" label="渠道选择">
+          <el-select v-model="value" placeholder="请选择渠道">
+            <el-option
+              v-for="item in region"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"></el-option>
+          </el-select>
+          <!-- <el-button  type="primary" plain @click="step2" style="margin-left: 10px">确 定</el-button> -->
+        </el-form-item>
         <el-form-item v-if="active===2" label="一键跑批">
-          <el-button  type="warning" plain @click="step3">一键跑批</el-button>
+
+          <el-button  type="primary" plain @click="step3">确 定</el-button>
         </el-form-item>
         <el-form-item v-if="active===3" label="一键跑批录屏结果" label-width="140px">
           <div class="tableau">一键跑批录屏</div>
         </el-form-item>
       </el-form>
     <div class="clear"></div>
+    	<!-- 加载中 -->
+		<div class="loading-bg" v-if="loading"><div class="loading-spinner">
+			<img src="../assets/loading.gif" alt="正在加载" class="loading-img">	
+		</div></div>
+  </div>
   </div>
 </template>
 
@@ -79,8 +95,9 @@ export default {
           { value: '4', label: '微信' },
           { value: '3', label: 'ob' },
         ],
-       active: 0,
-       value: ''
+      active: 0,
+      value: '',
+      loading: false
       }
 
   },
@@ -96,8 +113,19 @@ export default {
         }
       },
       step3() {
-        this.active = 3;
+        if (this.value) {
+          this.loading = true;
+          setTimeout(() => {
+            this.loading = false
+            this.active = 3;
+          }, 1000)
+
+        }else {
+          this.$message.error('请确保您已选择渠道')
+        }
       }
+
+      
     }
 }
 </script>
@@ -110,4 +138,23 @@ export default {
     height: 100px;
     border: 1px solid red;
  }
+ .loading-bg {
+	position: absolute;
+	z-index: 2000;
+	background-color: hsla(0,0%,100%,.75);
+	margin: 0;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	transition: opacity .3s;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.loading-img {
+	width: 300px;
+	height: 300px;
+	border-radius: 50%;
+}
 </style>
